bd769656f95ec411618fcecbf18b950fcb8b29d2
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-04-28 20:54:23 -0300

    `Update QuizzComponent HTML and TS files with title and question display changes`

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -9,6 +9,17 @@ export class QuizzComponent {
 
   title:string = ""
 
+  questions:any
+  questionsSelected:any
+
+  answers:string[] = []
+  answersSelected:string = ""
+
+  questionIndex:number = 0
+  questionMaxIndex:number = 0
+
+  finished:boolean = false
+
   constructor(){
 
   }
